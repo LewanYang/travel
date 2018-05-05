e145c60e8ec60fe2877ab8403e0dca1a2b326762
lewanyang <3278353995@qq.com>
2018-05-05 22:31:36 +0800

    头部组件的编写

---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe61a;</span>
+      <input type="text" placeholder="输入城市/景点/游玩主题">
+    </div>
+    <div class="header-right">
+      城市 <span class="iconfont">&#xe62d;</span>
+    </div>
+  </div>
+</template>
+<script>
+  export default{}
+</script>
+<style lang="stylus" scoped>
+  /* 1rem = 50px* 由于看的图片是2倍图，所以这里的height实际是43px */
+  @import "~@style/varibles.styl"
+  .header
+    line-height: .86rem
+    display: flex
+    background-color: $bgColor
+    color: #ffffff
+    .header-left
+      width: .64rem
+      float: left
+      text-align: center
+      padding-left .1rem
+    .header-input
+      flex: 1
+      height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      background #fff
+      border-radius: .1rem
+      color: #ccc
+      line-height .64rem
+      text-indent .1rem
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '../pages/home/home.vue'
 
 Vue.use(Router)
 
@@ -10,5 +11,11 @@ export default new Router({
     //   name: 'HelloWorld',
     //   component: HelloWorld
     // }
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    }
+
   ]
 })
