c62bc3f1a4b11f3d8c4701d9ff5b48898bad5694
lewanyang <3278353995@qq.com>
2018-07-31 20:35:15 +0800

    banner

---
+++ src/pages/detail/Detail.vue
@@ -0,0 +1,15 @@
+<template>
+  <detail-banner></detail-banner>
+</template>
+<script>
+import DetailBanner from './components/Banner.vue'
+export default {
+  components: {
+    DetailBanner
+  }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
+
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="banner">
+    <img src="http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_800x800_9ef05ee7.png" alt="新垣结衣"
+    class="banner-img">
+    <div class="img-info">
+      <div class="banner-title"></div>
+      <div class="banner-number"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="stylus" scoped>
+  .banner
+    overflow: hidden;
+    height: 0;
+    padding-bottom: 55%;
+    .banner-img
+      width 100%
+</style>
+
+
